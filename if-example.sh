39a127b0d5f5fa87be6cee4bb731b373116bd7c2
hyeonsu0 <70935190+hyeonsu0@users.noreply.github.com>
2021-03-22 19:45:30 +0900

    Update if-example.sh

---
+++ if-example.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#2021-03-17 Soo
+
+read -p "Enter Income Amount: " Income
+read -p "Enter Expenses Amount: " Expense
+let Net=$Income-$Expense
+if [ "$Net" -eq "0" ]; then
+echo "Income and Expenses are equal - breakeven."
+elif [ "$Net" -gt "0" ]; then
+echo "Profit of: " $Net
+else
+echo "Loss of: " $Net
+fi
